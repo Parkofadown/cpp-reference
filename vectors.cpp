cdfdd04a702c8906164165b7b2c87b5c49fcf3e9
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-06-28 02:47:01 -0400

    Add files via upload

---
+++ vectors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+  vector <char> vowels {'a','e','i','o','u'};
+  int i;
+  for ( i = 0; i < 5; i++)
+  {
+      cout << " " << vowels[i];
+  }
+  
+}
