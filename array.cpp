312f1ae415acc8afc6176e774e126f412371ea7f
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-06-28 02:42:46 -0400

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a [5];
+    int b [5] {2,4,6,8,10};
+    int i;
+
+    for ( i = 0; i < 5; i++)
+    {
+        cout << " " << b[i] ;
+    }
+    
+
+}
